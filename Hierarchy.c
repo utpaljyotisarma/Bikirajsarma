e037b9e121fedfc3a60b2a8a50d0bd918622eade
utpaljyotisarma <utpaljyotisarmasarma@gmail.com>
2024-12-29 17:22:27 +0530

    Add files via upload

---
+++ Hierarchy.c
@@ -0,0 +1,14 @@
+/*Output of the following program*/
+#include<stdio.h>
+int main()
+{
+    int i;
+    printf("Ente value of i:");
+    scanf("%d", &i);
+    if(i=5)
+        printf("You entered 5\n");
+    else
+        printf("You entered something other than 5\n");
+
+        return 0;
+}
