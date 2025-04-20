58b6a7a2633a59a9f87d3387eda36a6b0f0b2a0b
utpaljyotisarma <utpaljyotisarmasarma@gmail.com>
2025-04-20 20:01:22 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,24 @@
+/*Calculate average of the numbers between M and N. alue of M and N wi!! be entered 
+through keyboard. */
+#include<stdio.h>
+int main()
+{
+    int n, i;
+    float num[100], sum=0.0,avg;
+    printf("Ener the numbers if elements:");
+    scanf("%d", &n);
+    while(n>100||n<1){
+        printf("Integer number should in range of (1 to 100).\n");
+        printf("Enter the number again:");
+        scanf("%d",&n);
+    }
+    for(i=0; i<n; ++i)
+    {
+        printf("%d. Integer number:",i+1);
+        scanf("%f",&num[i]);
+        sum += num[i];
+    }
+    avg = sum/n;
+    printf("Average = %.2f", avg);
+    return 0;
+}
